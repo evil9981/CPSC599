f4e88ee8c89f547dc1a5b6ec3b9bacb54c36ebfe
Eric <ninuson123@gmail.com>
2015-10-29 14:08:23 -0600

    Removed the defender scene, I think we should use one scene for everything in game related. Added camera that works off mouse drags. Added artificial boundaries to the scene so that the bounding box of the camera (the scene size) will be easier to visualize.

--- GameApp/AttackerScene.swift
+++ GameApp/AttackerScene.swift
@@ -11,6 +11,13 @@ import SpriteKit
 
 class AttackerScene: SKScene
 {
+    var sceneCamera : SKCameraNode = SKCameraNode()
+    
+    var max_x : CGFloat = 0.0
+    var min_x : CGFloat = 0.0
+    var max_y : CGFloat = 0.0
+    var min_y : CGFloat = 0.0
+    
     override func didMoveToView(view: SKView)
     {
         backgroundColor = SKColor.greenColor()
@@ -24,7 +31,28 @@ class AttackerScene: SKScene
         
         addChild(myLabel)
         
+        sceneCamera.position = CGPointMake(scene!.frame.width/2, scene!.frame.height/2)
+        sceneCamera.xScale = 0.5
+        sceneCamera.yScale = 0.5
+        self.addChild(sceneCamera)
+        
+        let camera_viewport_width = self.scene!.frame.width * sceneCamera.xScale
+        let camera_viewport_height = self.scene!.frame.height * sceneCamera.yScale
+        
+        let camera_half_width = (camera_viewport_width / 2 )
+        let camera_half_height = (camera_viewport_height / 2 )
+
+        
+        max_x = scene!.frame.width - camera_half_width
+        min_x = 0 + camera_half_width
+        
+        max_y = scene!.frame.height - camera_half_height
+        min_y = 0 + camera_half_height
+        
+        self.camera = sceneCamera;
     }
+    
+    var prevLocation: CGPoint = CGPointMake(0, 0)
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
     {
@@ -34,24 +62,52 @@ class AttackerScene: SKScene
             let scenePoint = scene!.convertPointFromView(touchPoint)
             let touchedNode = self.nodeAtPoint(scenePoint)
             
+            prevLocation = touchPoint
+            
             if let name = touchedNode.name
             {
                 if name == "ToDefender"
                 {
-                    loadDefenderScene()
+                    debugPrint("Button clicked")
                 }
             }
         }
     }
     
-    func loadDefenderScene()
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?)
     {
-        if let scene = DefenderScene(fileNamed:"DefenderScene")
+        for touch in touches
         {
-            scene.scaleMode = scaleMode
+            let currentLocation = touch.locationInView(self.view)
             
-            let reveal = SKTransition.fadeWithDuration(1)
-            self.view?.presentScene(scene, transition: reveal)
+            let dx = currentLocation.x - prevLocation.x
+            let dy = currentLocation.y - prevLocation.y
+
+            prevLocation = currentLocation
+
+            var new_x = sceneCamera.position.x - dx * 2.5
+            var new_y = sceneCamera.position.y + dy*2.5
+            
+            if new_x > max_x
+            {
+                new_x = max_x
+            }
+            else if new_x < min_x
+            {
+                new_x = min_x
+            }
+            
+            if new_y > max_y
+            {
+                new_y = max_y
+            }
+            else if new_y < min_y
+            {
+                new_y = min_y
+            }
+            
+            sceneCamera.position = CGPointMake( new_x, new_y )
+            
         }
     }
 }
