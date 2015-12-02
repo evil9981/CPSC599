ff923fb89d1bf318ebbb99412b661fbbe7dfec02
Roger Sun <roger.sun.2008@gmail.com>
2015-12-02 08:49:26 -0700

    added sprites for troll and goblin as well as corresponding unit class

---
+++ Goblin.swift
@@ -0,0 +1,32 @@
+//
+//  Goblin.swift
+//  GameApp
+//
+//  Created by Roger Sun on 2015-12-02.
+//  Copyright © 2015 Eric. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class Goblin: Unit
+{
+    static let leftTextures = [SKTexture(imageNamed: "goblin_left_0"), SKTexture(imageNamed: "goblin_left_1"), SKTexture(imageNamed: "goblin_left_0"), SKTexture(imageNamed: "goblin_left_2")]
+    
+    static let rightTextures = [SKTexture(imageNamed: "goblin_right_0"), SKTexture(imageNamed: "goblin_right_1"), SKTexture(imageNamed: "goblin_right_0"), SKTexture(imageNamed: "goblin_right_2")]
+    
+    static let upTextures = [SKTexture(imageNamed: "goblin_up_0"), SKTexture(imageNamed: "goblin_up_1"), SKTexture(imageNamed: "goblin_up_0"), SKTexture(imageNamed: "goblin_up_2")]
+    
+    static let downTextures = [SKTexture(imageNamed: "goblin_down_0"), SKTexture(imageNamed: "goblin_down_1"), SKTexture(imageNamed: "goblin_down_0"), SKTexture(imageNamed: "goblin_down_2")]
+    
+    init(scene: GameScene, grid_position: int2, world_position: CGPoint, speed: NSTimeInterval)
+    {
+        let gridSize = int2(1,1)
+        let visSize = CGPointMake(1.2, 1.2)
+        
+        super.init(scene: scene, grid_position: grid_position, world_position: world_position,
+            leftTextures: Goblin.leftTextures, rightTextures: Goblin.rightTextures,
+            upTextures: Goblin.upTextures, downTextures: Goblin.downTextures,
+            speed: speed, gridSize: gridSize, visSize: visSize)
+    }
+}
---
+++ Troll.swift
@@ -0,0 +1,33 @@
+//
+//  Troll.swift
+//  GameApp
+//
+//  Created by Roger Sun on 2015-12-02.
+//  Copyright © 2015 Eric. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class Troll: Unit
+{
+    static let leftTextures = [SKTexture(imageNamed: "troll_left_0"), SKTexture(imageNamed: "troll_left_1"), SKTexture(imageNamed: "troll_left_0"), SKTexture(imageNamed: "troll_left_2")]
+    
+    static let rightTextures = [SKTexture(imageNamed: "troll_right_0"), SKTexture(imageNamed: "troll_right_1"), SKTexture(imageNamed: "troll_right_0"), SKTexture(imageNamed: "troll_right_2")]
+    
+    static let upTextures = [SKTexture(imageNamed: "troll_up_0"), SKTexture(imageNamed: "troll_up_1"), SKTexture(imageNamed: "troll_up_0"), SKTexture(imageNamed: "troll_up_2")]
+    
+    static let downTextures = [SKTexture(imageNamed: "troll_down_0"), SKTexture(imageNamed: "troll_down_1"), SKTexture(imageNamed: "troll_down_0"), SKTexture(imageNamed: "troll_down_2")]
+    
+    init(scene: GameScene, grid_position: int2, world_position: CGPoint, speed: NSTimeInterval)
+    {
+        let gridSize = int2(1,1)
+        let visSize = CGPointMake(1.2, 1.2)
+        
+        super.init(scene: scene, grid_position: grid_position, world_position: world_position,
+            leftTextures: Troll.leftTextures, rightTextures: Troll.rightTextures,
+            upTextures: Troll.upTextures, downTextures: Troll.downTextures,
+            speed: speed, gridSize: gridSize, visSize: visSize)
+    }
+}
+
